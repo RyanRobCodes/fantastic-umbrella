731943b61ca3d81e26d29ca170f7009caa858cc0
Ryanrobcodes <ryan.robinsonlb@gmail.com>
2022-05-11 21:05:58 -0400

    second commit

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,8 +9,52 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  },
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },  
+
+    product_name: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.STRING,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+    },
+
+    price: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.DECIMAL,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      isDecimal: true
+    },
+
+    stock:{
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        defaultValue: 10,
+        isNumeric: true
+    },
+    
+    category_id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      },
+
+    },      
   {
     sequelize,
     timestamps: false,
--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 const routes = require('./routes');
 // import sequelize connection
 
@@ -8,9 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+//turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now Listening'));
 });
